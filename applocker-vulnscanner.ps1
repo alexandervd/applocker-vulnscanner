3fa90ae84fabc60edeab4b4c0fe8e0226bc9bf56
Simon Bockaert <simon.bockaert@mil.be>
2018-10-17 15:35:45 +0200

    start again

---
+++ applocker-vulnscanner.ps1
@@ -0,0 +1,72 @@
+<#
+    Script that scans the applocker policy for flawed path rules
+    Authors: Alexander Van Daele, Simon Bockaert
+    License: GNU General Public License v3.0
+    Required Dependencies: None
+    Optional Dependencies: None
+#>
+
+function Check-ApplockerFolder {
+    <#
+    .SYNOPSIS
+    Writes customized output to a host.
+    .DESCRIPTION
+    The Write-Host cmdlet customizes output. You can specify the color of text by using
+    the ForegroundColor parameter, and you can specify the background color by using the
+    BackgroundColor parameter. The Separator parameter lets you specify a string to use to
+    separate displayed objects. The particular result depends on the program that is
+    hosting Windows PowerShell.
+    #>
+}
+
+function Check-ApplockerFile {
+    <#
+    .SYNOPSIS
+    Writes customized output to a host.
+    .DESCRIPTION
+    The Write-Host cmdlet customizes output. You can specify the color of text by using
+    the ForegroundColor parameter, and you can specify the background color by using the
+    BackgroundColor parameter. The Separator parameter lets you specify a string to use to
+    separate displayed objects. The particular result depends on the program that is
+    hosting Windows PowerShell.
+    #>
+}
+
+function Check-ApplockerPath {
+    <#
+    .SYNOPSIS
+    Writes customized output to a host.
+    .DESCRIPTION
+    The Write-Host cmdlet customizes output. You can specify the color of text by using
+    the ForegroundColor parameter, and you can specify the background color by using the
+    BackgroundColor parameter. The Separator parameter lets you specify a string to use to
+    separate displayed objects. The particular result depends on the program that is
+    hosting Windows PowerShell.
+    #>
+}
+
+function Get-ApplockerPaths {
+    <#
+    .SYNOPSIS
+    Writes customized output to a host.
+    .DESCRIPTION
+    The Write-Host cmdlet customizes output. You can specify the color of text by using
+    the ForegroundColor parameter, and you can specify the background color by using the
+    BackgroundColor parameter. The Separator parameter lets you specify a string to use to
+    separate displayed objects. The particular result depends on the program that is
+    hosting Windows PowerShell.
+    #>
+}
+
+function Check-ApplockerFlaws {
+    <#
+    .SYNOPSIS
+    Writes customized output to a host.
+    .DESCRIPTION
+    The Write-Host cmdlet customizes output. You can specify the color of text by using
+    the ForegroundColor parameter, and you can specify the background color by using the
+    BackgroundColor parameter. The Separator parameter lets you specify a string to use to
+    separate displayed objects. The particular result depends on the program that is
+    hosting Windows PowerShell.
+    #>
+}
